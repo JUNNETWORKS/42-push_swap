a648250b2a85bf4f89315eac33e0648985b167be
Junichi Tanaka <junichi200123@gmail.com>
2021-06-05 16:18:02 +0900

    add: e_operations

--- push_swap.h
+++ push_swap.h
@@ -1,6 +1,25 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+enum e_operations {
+	OP_SA = 0x0,
+	OP_SB,
+	OP_SS,
+	OP_PA,
+	OP_PB,
+	OP_RA,
+	OP_RB,
+	OP_RR,
+	OP_RRA,
+	OP_RRB,
+	OP_RRR
+};
+
+/* t_dlist is used as stack
+ *
+ * dummy->next is top of stack
+ * dummy->prev is bottom of stack
+ */
 typedef struct		s_dlist {
 	int				val;
 	struct s_dlist	*next;
