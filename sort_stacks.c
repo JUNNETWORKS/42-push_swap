8775d6f73c53feda53b09ed9e008eb40a071dca0
Junichi Tanaka <junichi200123@gmail.com>
2021-07-13 15:25:08 +0900

    pivotをベースにスタックA,Bに分割するpartition_stacks()を実装

--- sort_stacks.c
+++ sort_stacks.c
@@ -3,6 +3,31 @@
 #include "libft/libft.h"
 #include "push_swap.h"
 
+void	partition_stacks(t_dlist *dummy_a, t_dlist *dummy_b, int pivot, int len)
+{
+	t_dlist *current;
+	t_dlist *next;
+	int i;
+
+	i = 0;
+	current = dummy_a->next;
+	while (i < len)
+	{
+		next = current->next;
+		if (current->val <= pivot)
+			push_b(dummy_a, dummy_b);
+		else
+			dlist_rotate(dummy_a);
+		current = next;
+		i++;
+	}
+}
+
+void	quick_sort_stack(t_dlist *dummy_a, t_dlist *dummy_b, int *arr, int unsorted_num)
+{
+	partition_stacks(dummy_a, dummy_b, arr[unsorted_num / 2], unsorted_num);
+}
+
 void	sort_many_elements(t_dlist *dummy_a, t_dlist *dummy_b)
 {
 	int	*arr;
@@ -15,6 +40,7 @@ void	sort_many_elements(t_dlist *dummy_a, t_dlist *dummy_b)
 	sort_arr(arr, dlist_len(dummy_a));
 	for (int i = 0; i < dlist_len(dummy_a); i++)
 		printf("arr[%d]: %d\n", i, arr[i]);
+	quick_sort_stack(dummy_a, dummy_b, arr, dlist_len(dummy_a));
 	return ;
 }
 
