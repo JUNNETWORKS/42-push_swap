6c1e6d8e381b509ab2b8d0fd798de68ea716d07e
Junichi Tanaka <junichi200123@gmail.com>
2021-07-18 15:12:37 +0900

    テスター修正
    要素数3つの時に途中でバグってた

--- test_push_swap.sh
+++ test_push_swap.sh
@@ -13,18 +13,20 @@ TEST_NUM=100
 for i in $(seq 1 "$TEST_NUM")
 do
   ARG=$(seq 1 1000 | shuf | head -n "${ARG_NUM}")
-  RESULT_OPS=$(./push_swap $ARG 2>/dev/null)
 
   # check if operations is correct
-  echo "$RESULT_OPS" | ./checker_linux $ARG
-  if [[ $? != 0 ]]
+  set +e
+  ./push_swap $ARG 2>/dev/null | ./checker_linux $ARG
+  IS_OK=$?
+  set -e
+  if [ $IS_OK -ne 0 ]
   then
     echo "operations isn't correct!"
     echo "ARG=\"${ARG}\""
     exit 1
   fi
 
-  RESULT=$(echo "$RESULT_OPS" | wc -l)
+  RESULT=$(./push_swap $ARG 2>/dev/null | wc -l)
   echo "${i}: RESULT: ${RESULT}"
   if (( RESULT > MAX ))
   then
