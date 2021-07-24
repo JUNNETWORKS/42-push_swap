e7a30c3499cbfcadbbc2cb9b890759edfb30e65b
Junichi Tanaka <junichi200123@gmail.com>
2021-07-24 17:25:42 +0900

    argcの上限を撤廃した

--- main.c
+++ main.c
@@ -8,9 +8,18 @@ static void	init_stacks(t_stacks *stacks)
 	stacks->dummy_a = create_dlist(0);
 	stacks->dummy_b = create_dlist(0);
 	stacks->dummy_ops = create_dlist(0);
+	stacks->sorted_arr = NULL;
 	stacks->pivot_count = 0;
 	if (!(stacks->dummy_a && stacks->dummy_b && stacks->dummy_ops))
 		exit(1);
+}
+
+static void	free_stacks(t_stacks *stacks)
+{
+	free_dlist(stacks->dummy_a);
+	free_dlist(stacks->dummy_b);
+	free_dlist(stacks->dummy_ops);
+	free(stacks->sorted_arr);
 }
 
 int	main(int argc, char **argv)
@@ -18,9 +27,10 @@ int	main(int argc, char **argv)
 	t_stacks	stacks;
 
 	init_stacks(&stacks);
-	if (argc <= 1 || argc > 10000 || !parse_argv(stacks.dummy_a, argv + 1))
+	if (argc <= 1 || !parse_argv(stacks.dummy_a, argv + 1))
 	{
 		ft_putendl_fd("Error", STDERR_FILENO);
+		free_stacks(&stacks);
 		return (1);
 	}
 	stacks.sorted_arr = dlist2arr(stacks.dummy_a);
@@ -29,8 +39,4 @@ int	main(int argc, char **argv)
 	sort_stacks(&stacks);
 	optimize_operations(&stacks);
 	print_all_operations(&stacks);
-	free_dlist(stacks.dummy_a);
-	free_dlist(stacks.dummy_b);
-	free_dlist(stacks.dummy_ops);
-	free(stacks.sorted_arr);
 }
